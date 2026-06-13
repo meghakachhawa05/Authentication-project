b1f21d828dc02a8f0dcc175265f57359e81e2e72
Megha kachhawa <160737025+meghakachhawa05@users.noreply.github.com>
2026-06-13 23:44:20 +0530

    Update settings.py

--- Authentication/settings.py
+++ Authentication/settings.py
@@ -24,7 +24,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-jp^gyl#pr3*d+*c9tmpgs!rqc6@4&b-s+&jdyt3s19=e7&-8qx"
+SECRET_KEY = os.environ.get('SECRET_KEY')
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
